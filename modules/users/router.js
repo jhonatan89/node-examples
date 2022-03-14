009f626acec8ff10b31a62823b0cb03a1ec44d81
galvisgrjp <galvisgrjp@gmail.com>
2022-03-14 14:23:07 -0500

    Add POST request for User entity

--- modules/users/router.js
+++ modules/users/router.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const { getAllUsers } = require('./controller');
+const { getAllUsers, createUser } = require('./controller');
 
-/* GET users listing. */
+/* GET users listing */
 router.get('/', async function (req, res, next) {
   const users = await getAllUsers();
   res.json(users);
 });
 
-/**
- * POST create user
- */
+/* POST create user */
+router.post('/', async function (req, res, next) {
+  const newUser = await createUser(req.body);
+  res.send(newUser);
+});
 
 module.exports = router;
