3bfde06ccef1282e081e0c0420c07e39f2d5ef5a
mvgarcia <mv.garcia@uniandes.edu.co>
2022-03-14 19:56:52 -0500

    mvgarcia

--- modules/users/router.js
+++ modules/users/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { getAllUsers } = require('./controller');
+const { getAllUsers, createUser } = require('./controller');
 
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
@@ -11,5 +11,10 @@ router.get('/', async function (req, res, next) {
 /**
  * POST create user
  */
+
+ router.post('/', async function (req, res, next) {
+  const newUser = await createUser(req.body);
+  res.send(newUser);
+});
 
 module.exports = router;
